351a70aa476da95c9e6b8fd11f8d1b4e38f2178a
See Yihui <cuoguo@gmail.com>
2018-11-26 09:30:03 +0800

    Update button component, variables and add mixins

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
-import { Button, Segment } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 import { addDecorator, storiesOf } from '@storybook/react';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
-interface IProps {
-    buttonText: string,
-    primary?: any,
-    secondary?: any,
-    basic?: any,
+interface IComponent {
+    className?: string
 }
-const CustomButton: React.SFC<IProps> = (props) => {
+
+export interface IProps extends IComponent {
+    name: string;
+    secondaryText?: string
+    type: 'primary' | 'ghost' | string;
+    length: 'fill' | 'fit' | 'responsive-fit' | string;
+    onClick?(): void;
+}
+const Button: React.SFC<IProps> = (props) => {
+
+    const className: string = `button ${props.type} ${props.length}`;
+
     return (
-        <Button {...props}>{props.buttonText}</Button>
-    )
-}
+        <button className={className} onClick={props.onClick}>
+            {props.name}
+            {props.secondaryText && <span className="secondary-text">{props.secondaryText}</span>}
+        </button>
+    );
+};
 
 addDecorator(withKnobs);
-storiesOf('Form/Button', module)
-    .addDecorator(story => <Segment>{story()}</Segment>)
-    .addWithJSX('Basic', () => <><CustomButton primary buttonText={text('Label', 'This is a button')}/><CustomButton secondary buttonText={text('Label', 'This is a button')}/></>)
+storiesOf('Button', module)
+    // .addDecorator(story => <Segment>{story()}</Segment>)
+    .addWithJSX('Basic', () => <><Segment><h3>Fill</h3><p>Button fills up the entire width of container</p><Button name='This is a fill button' type="primary" length='fill' /></Segment><Segment><h3>Fit</h3><p>Button width fits to size of text</p><Button name='This is a fit button' type="primary" length="fit" /></Segment><Segment><h3>Try it!</h3><p>Change the properties on the right to see how the button changes</p><Button name={text('Label', 'try to change the properties')} type={text('type', 'primary')} length={text('length', 'fit')} /></Segment></>)
