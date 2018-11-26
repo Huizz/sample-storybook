a9cde3b807067f7acc19a6ef1a3935ecd4a5181e
See Yihui <cuoguo@gmail.com>
2018-11-26 17:08:38 +0800

    Add grid component

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Grid as SemanticGrid, Segment } from 'semantic-ui-react';
+
+import { addDecorator, storiesOf } from '@storybook/react';
+import { withKnobs, object } from '@storybook/addon-knobs';
+
+interface IColumn {
+  columnWidth: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12'
+}
+
+interface IRow {
+  columns: IColumn[]
+}
+
+interface IProps {
+  rows?: IRow[];
+  columns: IColumn[];
+}
+
+import './Grid.scss';
+
+const generateSingleRowColumns = (columns: IColumn[]) => {
+  return columns.map((column, columnIndex) => {
+    return <SemanticGrid.Column computer={column.columnWidth}>
+      <div>{column.columnWidth}</div>
+    </SemanticGrid.Column>
+  })
+}
+
+const Grid: React.SFC<IProps> = (props) => {
+  return (
+    <SemanticGrid>
+      <SemanticGrid.Row>
+        {generateSingleRowColumns(props.columns)}
+      </SemanticGrid.Row>
+    </SemanticGrid>
+  )
+}
+
+export default Grid;
+
+const rowOneColumns = [
+  {
+    columnWidth: '12'
+  } as IColumn
+]
+
+const rowTwoColumns = [
+  {
+    columnWidth: '8'
+  } as IColumn,
+  {
+    columnWidth: '4'
+  } as IColumn
+]
+
+addDecorator(withKnobs);
+storiesOf('Grid', module)
+    .addWithJSX('Basic', () => {
+      const column_one: IColumn = object('column one', {
+        columnWidth: '3'
+      } as IColumn)
+      const column_two: IColumn = object('column two', {
+        columnWidth: '9'
+      } as IColumn)
+
+      const defaultColumns =  [column_one, column_two]
+
+      return (
+        <>
+          <Segment><h3>Full width Column</h3><Grid columns={rowOneColumns} /></Segment>
+          <Segment><h3>Columns</h3><Grid columns={rowTwoColumns} /></Segment>
+          <Segment><h3>Try it!</h3><Grid columns={defaultColumns} /></Segment>
+        </>
+      )
+    })
