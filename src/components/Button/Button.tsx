17c8940c565907b6d91ecf0a28ab086a4e85970b
See Yihui <cuoguo@gmail.com>
2018-11-28 17:56:29 +0800

    Add propTypes table for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,8 @@ import { Segment } from 'semantic-ui-react';
 import { addDecorator, storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
+import PropsTable from '../../utils/PropsTable';
+
 interface IComponent {
     className?: string
 }
@@ -26,7 +28,25 @@ const Button: React.SFC<IProps> = (props) => {
     );
 };
 
+const propTypes = [
+    {
+        type: 'name',
+        required: true,
+        description: 'The text that is displayed on the button'
+    },
+    {
+        type: 'secondaryText',
+        required: false,
+        description: 'The small line of text to display under the name'
+    },
+    {
+        type: 'type',
+        required: true,
+        description: 'The type of button. One of primary or ghost'
+    }
+]
+
 addDecorator(withKnobs);
 storiesOf('Button', module)
-    // .addDecorator(story => <Segment>{story()}</Segment>)
+.addDecorator(story => <Segment>{<PropsTable propTypes={propTypes} /> }{story()}</Segment>)
     .addWithJSX('Basic', () => <><Segment><h3>Fill</h3><p>Button fills up the entire width of container</p><Button name='This is a fill button' type="primary" length='fill' /></Segment><Segment><h3>Fit</h3><p>Button width fits to size of text</p><Button name='This is a fit button' type="primary" length="fit" /></Segment><Segment><h3>Try it!</h3><p>Change the properties on the right to see how the button changes</p><Button name={text('Label', 'try to change the properties')} type={text('type', 'primary')} length={text('length', 'fit')} /></Segment></>)
