a63dff02d5d3559607723d2ef38b791eb5baa24b
See Yihui <cuoguo@gmail.com>
2018-11-29 10:12:56 +0800

    Edit table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,3 +1,15 @@
+
+// declare module '@storybook/addon-viewport' {
+//   import * as addOnViewPort from '@storybook/addon-viewport';
+//   interface Helper {
+//     new(opt: addOnViewPort.Options): addOnViewPort.Type
+//   }
+//   export = Helper;
+//   // interface withViewport {
+//   //   any;
+//   // }
+//   // export = addOnViewPort.withViewport;
+// };
 import * as React from 'react';
 import { Table as SemanticUITable, Segment } from 'semantic-ui-react';
 import { addDecorator, storiesOf } from '@storybook/react';
@@ -49,5 +61,6 @@ storiesOf('Table', module)
   .addWithJSX('Basic', () => <Table id='test--table' 
     headerText={[text('Header cell', 'Header cell 1'), text('Header cell two', 'Child under 10 years old'), text('Header cell three', 'Adult')]} 
     cellRowText={[
-      [text('Cell 1', 'Cell 1'), text('Cell 2', 'Cell 2'), text('Cell 3', 'Cell 3')]
+      [text('Cell 1', 'Bacon ipsum dolor amet shankle landjaeger shank prosciutto.'), text('Cell 2', 'Cell 2'), text('Cell 3', 'Cell 3')],
+      [text('Row 2 cell 1', 'Bacon ipsum dolor amet shankle landjaeger shank prosciutto.'), text('Row 2 Cell 2', 'Cell 2'), text('Row 2 Cell 3', 'Cell 3')]
     ]} />)
