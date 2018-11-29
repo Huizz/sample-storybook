71dc5b0f997eb4f618d353c14e5e888541e2daeb
See Yihui <cuoguo@gmail.com>
2018-11-29 13:17:51 +0800

    Fix button component styling, add example for secondary text

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,6 +5,8 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 
 import PropsTable from '../../utils/PropsTable';
 
+import './Button.scss';
+
 interface IComponent {
     className?: string
 }
@@ -49,4 +51,25 @@ const propTypes = [
 addDecorator(withKnobs);
 storiesOf('Button', module)
 .addDecorator(story => <Segment>{<PropsTable propTypes={propTypes} /> }{story()}</Segment>)
-    .addWithJSX('Basic', () => <><Segment><h3>Fill</h3><p>Button fills up the entire width of container</p><Button name='This is a fill button' type="primary" length='fill' /></Segment><Segment><h3>Fit</h3><p>Button width fits to size of text</p><Button name='This is a fit button' type="primary" length="fit" /></Segment><Segment><h3>Try it!</h3><p>Change the properties on the right to see how the button changes</p><Button name={text('Label', 'try to change the properties')} type={text('type', 'primary')} length={text('length', 'fit')} /></Segment></>)
+    .addWithJSX('Basic', () => <>
+        <Segment>
+            <h3>Fill</h3>
+            <p>Button fills up the entire width of container</p>
+            <Button name='This is a fill button' type="primary" length='fill' />
+        </Segment>
+        <Segment>
+            <h3>Fit</h3>
+            <p>Button width fits to size of text</p>
+            <Button name='This is a fit button' type="primary" length="fit" />
+        </Segment>
+        <Segment>
+            <h3>With secondary text</h3>
+            <p>Button width fits to size of text</p>
+            <Button name='This is a fit button' type="primary" length="fit" secondaryText="This is secondary text" />
+        </Segment>
+        <Segment>
+            <h3>Try it!</h3>
+            <p>Change the properties on the right to see how the button changes</p>
+            <Button name={text('Label', 'try to change the properties')} type={text('type', 'primary')} length={text('length', 'fit')} />
+        </Segment>
+    </>)
