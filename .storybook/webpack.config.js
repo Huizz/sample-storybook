dad141de0f3200f39b156118bc506ae6a32a36c4
See Yihui <cuoguo@gmail.com>
2018-12-03 13:29:40 +0800

    Format webpack config file

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path');
 const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = (baseConfig, env, config) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('awesome-typescript-loader'),
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('awesome-typescript-loader'),
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
 
-    config.module.rules.push({
-        test: /\.scss$/,
-        loader: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../src')
-    });
+  config.module.rules.push({
+    test: /\.scss$/,
+    loader: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../src')
+  });
 
-    config.resolve.plugins = [new TsConfigPathsPlugin()];
-    
-    return config;
+  config.resolve.plugins = [new TsConfigPathsPlugin()];
+
+  return config;
 };
