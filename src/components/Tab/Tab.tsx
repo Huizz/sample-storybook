da7e101ed72b1b053a1c682026b699db9a252b85
See Yihui <cuoguo@gmail.com>
2018-12-06 13:22:06 +0800

    Edit tabs implementation

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -6,24 +6,41 @@ import './Tab.scss';
 import { Segment } from 'semantic-ui-react';
 import { storiesOf } from '@storybook/react';
 
-const generatePanes = () => {
-  return [
-    {
-      menuItem: 'My plans',
-      render: () => (<SemanticTab.Pane attached={false}>My plans content</SemanticTab.Pane>)
-    },
-    {
-      menuItem: 'Shared with me',
-      render: () => (<SemanticTab.Pane attached={false}>Shared with me</SemanticTab.Pane>)
-    }
-  ]
+interface ITabItem {
+  name: string;
+  content: React.ReactNode;
+}
+interface IProps {
+  tabs: ITabItem[]
 }
 
-const Tab: React.SFC<{}> = () => (
-  <SemanticTab className='eol-tab' panes={generatePanes()} />
+const generatePanes = (tabs: ITabItem[]) => {
+  return tabs.map((tab) => {
+     return {
+       menuItem: tab.name,
+       render: () => {
+         return tab.content;
+       }
+     }
+  });
+}
+
+const Tab: React.SFC<IProps> = (props) => (
+  <SemanticTab className='eol-tab' panes={generatePanes(props.tabs)} />
 )
 
 export default Tab;
+
+const tabs = [
+  {
+    name: 'My plans',
+    content: <div>My plans content</div>
+  },
+  {
+    name: 'Shared with me',
+    content: <div>Shared with me content</div>
+  }
+]
 
 storiesOf('Experiment', module)
   .addDecorator(story => <Segment>{story()}</Segment>)
@@ -32,7 +49,7 @@ storiesOf('Experiment', module)
       <>
         <Segment style={{border: '1px solid black'}}>
           <h3>Tab</h3>
-          <Tab />
+          <Tab tabs={tabs}/>
         </Segment>
       </>
     )
